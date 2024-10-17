892d99a6864d9970c54ec249810f794513b6ef8d
dgexi <dgexi@GEX-DESKTOP>
2024-10-17 18:13:01 +0200

    joystick compiles, not tested

--- keyboards/duo_board/left/keymaps/default/keymap.c
+++ keyboards/duo_board/left/keymaps/default/keymap.c
@@ -4,21 +4,23 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
+
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        KC_P7,   KC_P8,   KC_P9,  KC_PSLS,  KC_NO, KC_NO, KC_NO,
+        KC_P4,   KC_P5,   KC_P6,  KC_PAST,  KC_NO, KC_NO, KC_NO,
+        KC_P1,   KC_P2,   KC_P3,  KC_PMNS,  KC_NO, KC_NO, KC_NO,
+                                            KC_NO, KC_NO
     )
 };
+
+// void matrix_init_user(void) {
+//     // Set the pin as an input
+//     pinMode(B15, INPUT_PULLUP);
+// }
+
+// void matrix_scan_user(void) {
+//     // Check if the pin is LOW (key pressed)
+//     if (readPin(B15) == 0) {
+//         // Key is pressed, handle it
+//     }
+// }
---
+++ keyboards/duo_board/left/mcuconf.h
@@ -0,0 +1,4 @@
+#include_next <mcuconf.h>
+   
+#undef STM32_ADC_USE_ADC1
+#define STM32_ADC_USE_ADC1 TRUE
